1a477e88a7a5282c51c4e190f3ab2572d4f47192
Laniekea <jzkoay@gmail.com>
2024-10-05 18:26:37 +0800

    change response to XML format

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Swashbuckle.AspNetCore.SwaggerUI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using XMLCalculator;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,12 +24,43 @@ if (app.Environment.IsDevelopment())
 
 app.MapPost("/calculate", (string xml, CalculatorService calculatorService) =>
 {
-    try {
+    try
+    {
         var result = calculatorService.CalculateFromXml(xml);
-        return Results.Ok(new { Value = result, Unit = "cm"  });    
+
+        XDocument responseXml = new XDocument(
+        new XElement("Result",
+            new XElement("quantity", result),   // If using a DTO
+            new XElement("unit", "cm")      // If using a DTO
+        )
+    );
+
+        // Return XML with proper content type
+        return Results.Content(responseXml.ToString(), "application/xml", statusCode: 200);
     }
-    catch (Exception ex) {
-        return Results.BadRequest(new { Message = ex.Message });
+    catch (InvalidOperationException ex)
+    {
+
+        XDocument errorXml = new XDocument(
+            new XElement("Bad Request",
+                new XElement("Message", ex.Message)
+            )
+        );
+
+        // Return XML error response with status code 400
+        return Results.Content(errorXml.ToString(), "application/xml", statusCode: 400);
+    }
+    catch (Exception ex)
+    {
+
+        XDocument errorXml = new XDocument(
+            new XElement("Internal Server Error",
+                new XElement("Message", ex.Message)
+            )
+        );
+
+        // Return XML error response with status code 400
+        return Results.Content(errorXml.ToString(), "application/xml", statusCode: 500);
     }
 })
 .WithName("Calculate")
