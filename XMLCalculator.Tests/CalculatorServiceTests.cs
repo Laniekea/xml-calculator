52e38eab7cb955f7e1c7d99bc28c8470958d8503
Laniekea <jzkoay@gmail.com>
2024-10-05 17:09:19 +0800

    add xunit unit test

---
+++ XMLCalculator.Tests/CalculatorServiceTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using System;
+
+namespace XMLCalculator.Tests
+{
+    public class CalculatorServiceTests
+    {
+        private readonly CalculatorService _calculatorService;
+        public CalculatorServiceTests()
+        {
+            _calculatorService = new CalculatorService();
+        }
+
+        [Fact]
+        public void TestAddition()
+        {
+            // Arrange
+            string xml = @"<?xml version='1.0' encoding='UTF-8'?><Maths xmlns:calc='http://example.com/calc'><calc:Operation type='Addition'><calc:Operand unit='meters' quantity='5.5'/><calc:Operand unit='cm' quantity='3.25'/></calc:Operation></Maths>";
+
+            // Act
+            double result = _calculatorService.CalculateFromXml(xml);
+
+            // Assert
+            Assert.Equal(8.75, result, 2); // 5.5 + 3.25 = 8.75
+        }
+    }
+}
