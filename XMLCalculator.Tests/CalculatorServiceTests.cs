bbec09d9366209e44dd47fb83e3f9936fd1a115e
Laniekea <jzkoay@gmail.com>
2024-10-05 17:35:11 +0800

    add nested 2 lv operation test case

--- XMLCalculator.Tests/CalculatorServiceTests.cs
+++ XMLCalculator.Tests/CalculatorServiceTests.cs
@@ -36,5 +36,18 @@ namespace XMLCalculator.Tests
             // Assert
             Assert.Equal(15, result, 2); // 5*3 = 15
         }
+
+        [Fact]
+        public void TestNestedOperations()
+        {
+            // Arrange
+            string xml = @"<?xml version='1.0' encoding='UTF-8'?><Maths xmlns:calc='http://example.com/calc'><calc:Operation type='Addition' method='Recursive'><calc:Operand unit='meters' quantity='5'/><calc:Operand unit='cm' quantity='3'/><calc:Operation type='Multiplication' method='Normal'><calc:Operand unit='meters' quantity='2'/><calc:Operand unit='cm' quantity='4'/></calc:Operation></calc:Operation></Maths>";
+
+            // Act
+            double result = _calculatorService.CalculateFromXml(xml);
+
+            // Assert
+            Assert.Equal(14, result, 2); // (5+3) + (2*4) = 14
+        }
     }
 }
